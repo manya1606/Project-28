1fd7df7c47ba08b8e9a729f0141cca6b5ae015f1
manya1606 <71758041+manya1606@users.noreply.github.com>
2020-12-01 16:18:50 +0530

    Add files via upload

---
+++ MangoClass.js
@@ -0,0 +1,10 @@
+class Mango {
+    constructor(x,y,width,height) {
+      var options = {
+          isStatic: true,
+          restitution:0,
+          friction:1
+      }
+      this.mango = loadImage ("Plucking mangoes/mango.png")
+    }
+}
---
+++ TreeClass.js
@@ -0,0 +1,11 @@
+class Tree {
+    constructor(x,y,width,height) {
+      var options = {
+          isStatic: false,
+          restitution:0,
+          friction:1,
+          density:1.2
+      }
+      this.tree = loadImage ("Plucking mangoes/tree.png")
+    }
+}
